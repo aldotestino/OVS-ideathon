d8c4cd4b3873ee73c290f678419bf7ee14d7ad56
PaoloMasciullo <paolomasciullojz4@gmail.com>
2023-10-19 12:28:33 +0200

    sanity check of the length of input prompt

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from model import Model
@@ -15,6 +15,9 @@ class Prompt(BaseModel):
 
 @app.post("/generate_copy")
 def generate_copy(prompt: Prompt):
+    if len(prompt.message) < 10:
+        raise HTTPException(status_code=400, detail="Message too short")
+    prompt += " di massimo 250 caratteri"
     copy = model.predict(prompt.message)
     return {"copy": copy}
 
